d642496cd81cfbe2fc21c2b5943fe1d38db4471a
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-06-21 14:56:12 +0900

    chore : dependency 추가 및 환경설정 - 모듈별 분리된 환경설정 위해 Application.java에 setProperty 추가 - 전체 모듈 :     - 환경설정 : validation, lombok 추가 - domain :     - 환경설정 : db 연동     - 의존성 : mysql connector 추가 - api :     - 환경설정 : port 변경, devtools 추가     - 의존성 : mapstruct 추가

--- playground-api/src/main/java/yana/playground/PlaygroundApiApplication.java
+++ playground-api/src/main/java/yana/playground/PlaygroundApiApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//초기설정시에 임시로 설정한 값(DB연동 후 바로 삭제 필요)
+@SpringBootApplication//초기설정시에 임시로 설정한 값(DB연동 후 바로 삭제 필요)
 public class PlaygroundApiApplication {
     public static void main(String[] args) {
+        System.setProperty("spring.config.name", "application-api,application-domain,application-SECRET");
         SpringApplication.run(PlaygroundApiApplication.class, args);
     }
 }
