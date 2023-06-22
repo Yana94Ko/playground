d57455605dd80df5b2d57d1d18a40871fd6c1ddc
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-06-22 15:40:56 +0900

    refactor : property 주입 방식 수정
    - property를 main에 지정하는것이 아닌, yml 파일에서 include하는 방식으로 수정
    - profiles별 property 구분하여 지정하기 위해 yml 파일 내 profiles구분

--- playground-api/src/main/java/yana/playground/PlaygroundApiApplication.java
+++ playground-api/src/main/java/yana/playground/PlaygroundApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication//초기설정시에 임시로 설정한 값(DB연동 후 바로 삭제 필요)
 public class PlaygroundApiApplication {
     public static void main(String[] args) {
-        System.setProperty("spring.config.name", "application-api,application-domain,application-SECRET");
         SpringApplication.run(PlaygroundApiApplication.class, args);
     }
 }
